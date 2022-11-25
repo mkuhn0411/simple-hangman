f3ba9e63ee44966915d8e2b3501b759cd529ec26
Mel <91561937+mkuhn0411@users.noreply.github.com>
2022-11-25 03:49:10 +0000

    made lives a local variable

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import hangman_words
 chosen_word = random.choice(hangman_words.word_list)
 display = []
 guess = ""
-lives = 6
 
 print(hangman_art.logo)
 
@@ -22,7 +21,6 @@ def get_user_guess():
 
 #determines if guess is true, updates display, lives if needed
 def handle_guess(user_guess):
-  global lives
   correct_guess = False
 
   if user_guess in display:
@@ -31,36 +29,37 @@ def handle_guess(user_guess):
   for idx, letter in enumerate(chosen_word):
     if letter == user_guess:
       display[idx] = letter
-      correct_guess = True
-  #if guess is not correct add to count
-  if not correct_guess:
-    lives -= 1
-    
+      correct_guess = True    
   return correct_guess
 
-def display_result(guess_is_true):
+def display_result(guess_is_true, lives_left):
   if guess_is_true and '_' in display:
       print("Correct!")
       print(display)
   elif guess_is_true and '_' not in display:
       print("You win!!!")
-  elif not guess_is_true and lives > 0:
+  elif not guess_is_true and lives_left > 0:
     print("Incorrect correct, you lose a life.")
-    print(hangman_art.stages[lives])
+    print(hangman_art.stages[lives_left])
   else:
-    print(hangman_art.stages[lives])
+    print(hangman_art.stages[lives_left])
     print("Sorry, you are dead!")
     
   
 def run():
+  lives = 6
+  
   while '_' in display and lives > 0:
     guess = get_user_guess()
     is_correct_guess = handle_guess(guess)
 
+    if not is_correct_guess:
+      lives -= 1
+
     if is_correct_guess == 'duplicate':
       print("You have already guessed this. Pick another letter")
     else:
-      display_result(is_correct_guess)
+      display_result(is_correct_guess, lives)
 
 #initial start
 print(display)
